564fe387cbba9567c799af0492c95161ee355ec9
BrianRichmond <brian.richmond@gmail.com>
2018-10-29 19:13:35 -0700

    initial

---
+++ Network_Analysis_code.R
@@ -0,0 +1,9 @@
+## Organizational Network Analysis (ONA)
+##    of an anonymized email network at a large European research institution
+##
+##  
+##
+
+### import data on node attributes
+nodes <- read.table("email-Eu-core-department-labels.txt", header = F)
+head(nodes)
